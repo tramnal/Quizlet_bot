850cb2e5c106ccfade9e54cab2de826c9ab211b7
tramnal <tramnal@hotmail.com>
2025-05-30 00:47:18 +0300

    deleted db.py and created connect_to_db.py

---
+++ app/database/connect_to_db.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session = async_sessionmaker(engine)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+async def get_session():
+    async with async_session() as session:
+        yield session
