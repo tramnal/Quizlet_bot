4e04bc4332582b6570cf56a5d243596a818c264e
tramnal <tramnal@hotmail.com>
2025-05-30 13:46:33 +0300

    described dictionary.py

--- app/utils/dictionary.py
+++ app/utils/dictionary.py
@@ -1,12 +1,67 @@
 import aiohttp
+from typing import Optional
 
 
-async def get_user_word_data(word: str) -> dict | None:
-    '''Trying to find word's data in dictionary API'''
+class UserWordData:
+    def __init__(self, word: str):
+        self.word = word
+        self.dictionary_url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
+        self.translation_url = f'https://libretranslate.com/translate'
 
-    url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
-    async with aiohttp.ClientSession as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                return await resp.json()
+    async def get_word_data(self) -> Optional[dict]:
+        '''Trying to get data from dictionaryapi.dev'''
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.dictionary_url) as resp:
+                if resp.status == 200:
+                    data = resp.json()
+                    return data[0] if data else None
+                return None
+    
+    async def get_word_translation(self) -> Optional[dict]:
+        '''Trying to find word's translation in libretranslate.com API'''
+
+        payload = {
+            'q': self.word,
+            'source': 'en',
+            'target': 'ru',
+            'format': 'text'
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.translation_url, json=payload) as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                    return data.get('translatedText')
+                return None
+            
+    async def get_word_full_data(self) -> Optional[dict]:
+        '''Returns dict includes word's transcription, translation, example & audio link'''
+
+        data = await self.get_word_data()
+        if not data:
             return None
+        
+        transcription = None
+        example = None
+        audio_url = None
+
+        phonetics = data.get('phonetics')
+        if phonetics:
+            transcription = phonetics[0].get('text')
+            audio_url = phonetics[0].get('audio')
+        
+        meanings = data.get('meanings')
+        if example:
+            examples = meanings[0].get('definitions')
+            if examples:
+                example = examples[0].get('example')
+        
+        translation = await self.get_word_translation()
+
+        return {
+            "word": self.word,
+            "transcription": transcription,
+            "translation": translation,
+            "example": example,
+            "audio_url": audio_url
+        }
